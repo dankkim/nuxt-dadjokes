c4d584ac4d52c8cdf0168634f948779f63d83396
Young-ae Kim <49858684+dankkim@users.noreply.github.com>
2022-02-25 15:05:06 +0900

    c048844

---
+++ _nuxt/static/1645769073/about/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-dadjokes\u002F_nuxt\u002Fstatic\u002F1645769073",layout:"default",error:a,serverRendered:true,routePath:"\u002Fabout",config:{app:{basePath:"\u002Fnuxt-dadjokes\u002F",assetsPath:"\u002Fnuxt-dadjokes\u002F_nuxt\u002F",cdnURL:a}}}}(null));
